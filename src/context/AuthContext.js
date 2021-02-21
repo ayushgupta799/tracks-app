20c7dfa623fffb26ca9a548fc8bbca2ad05e11ba
Ayush Gupta <guptaayush782@outlook.com>
2021-02-21 20:15:04 +0530

    Signup process with async storage

---
+++ src/context/AuthContext.js
@@ -0,0 +1,40 @@
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
+        default:
+            return state;
+    }
+};
+
+const signup = dispatch => {
+    return async ({email, password}) => {
+        try {
+            const response = await trackerApi.post('/signup', {email, password});
+            console.log(response.data);
+        } catch (err) {
+            dispatch({type: 'add_error', payload:'Something went wrong'})
+        }
+    };
+}; 
+
+const signin = dispatch => {
+    return ({email, password}) => {
+
+    };
+};
+
+const signout = dispatch => {
+    return () => {
+
+    };
+};
+
+export const { Provider, Context } = createDataContext(
+    authReducer,
+    {signin , signout, signup },
+    { isSignedIn: false, errorMessage :''}
+);
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import {View,StyleSheet,Text,Button} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View,StyleSheet} from 'react-native';
+import {Text,Input,Button} from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
 
 const SignupScreen = ({navigation}) => {
+    const {state ,signup } = useContext(AuthContext);
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
     return (
-        <>
-        <Text style={{fontSize:48}}>SignupScreen</Text>
-        <Button title="Go to Sign In Screen" onPress={() => navigation.navigate('Signin')} />
-        </>
+        <View style={styles.container} >
+            <Spacer>
+                <Text h3>Sign up for Tracker</Text>
+            </Spacer>
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <Spacer />
+            <Input
+                secureTextEntry
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
+            <Spacer>
+                <Button title="Sign Up" onPress={() => signup({email, password})} />
+            </Spacer>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    };
+};
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1,
+        justifyContent:'center',
+        marginBottom: 250
+    },
+    errorMessage:{
+        fontSize:16,
+        color:'red',
+        marginLeft:15,
+        marginTop:15
+    }
+});
 
 export default SignupScreen;
